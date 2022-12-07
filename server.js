dcc800cf3b51945c465f7830ae7e912c6a260b9c
Evgeniy Abashin <aevgen.001@yandex.ru>
2022-12-07 19:44:37 +0300

    Changed server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
-app.use(express.static('./'));
+app.use(express.static(`${__dirname}/`));
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
