6691e3b91b3247d66f6e0f15dd1b6e7e997fd39d
Evgeniy Abashin <aevgen.001@yandex.ru>
2022-12-08 22:05:11 +0300

    Fixed bugs with git branches

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-<<<<<<< HEAD
 app.use(express.static(`dist`));
-=======
-app.use(express.static(`${__dirname}/`));
->>>>>>> sprint_1
 
 app.listen(process.env.PORT || PORT, function () {
     console.log(`Example app listening on port ${process.env.PORT || PORT}!`);
